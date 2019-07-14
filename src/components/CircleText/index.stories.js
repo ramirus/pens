2d3a31c856c86d3dccdd5ae472db03033733fb3c
Ramiru <blackhawk111@yandex.ru>
2019-07-14 18:51:37 +0300

    add circle text

---
+++ src/components/CircleText/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import CircleText from './index';
+
+storiesOf('CircleText', module)
+  .add('circle', () => (
+    <CircleText />
+  ))
